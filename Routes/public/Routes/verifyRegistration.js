a38334781f621603d1616085c488ce915e5c2b1a
Henit Chobisa <henitchobisa0885@gmail.com>
2022-01-30 16:43:22 +0530

    vndjkns

--- Routes/public/Routes/verifyRegistration.js
+++ Routes/public/Routes/verifyRegistration.js
@@ -1,13 +1,14 @@
+
 const express = require("express");
 const QuestionSet = require("../../../Models/QuestionSetModel");
 const Registration = require("../../../Models/RegistrationModel");
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const { email } = req.body;
+    var { email } = req.body;
     email = email.trim();
     if (email != null){
-        var details = await Registration.findOne({ email });
+        var details = await Registration.findOne({ email }).populate('slot', 'code timing isActive');
         if ( details == null )
         {
             res.sendStatus(403);
--- Routes/public/public.js
+++ Routes/public/public.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use('/addRegistrations', require('./Routes/AddRegistrations'));
-router.use('./verify', require('./Routes/verifyRegistration'));
+router.use('/verify', require('./Routes/verifyRegistration'));
 
 module.exports = router;
