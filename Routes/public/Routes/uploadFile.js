17203c7e230ec5bb38968aed93173f25059e8e51
Henit Chobisa <henitchobisa0885@gmail.com>
2022-01-31 18:24:00 +0530

    fnbdsjnflds

--- Routes/public/Routes/uploadFile.js
+++ Routes/public/Routes/uploadFile.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+const ObjectID = require('mongoose').Types.ObjectId;
 const {upload, s3} = require('../../../Config/uploadSystem');
 const Registration = require('../../../Models/RegistrationModel');
 
 
 router.post('/',upload.single("Paper") ,async(req, res) => {
     const id = req.body.id
-    var reg = await Registration.findById(id);
-    reg.hasUploaded = true,
-    reg.hasCompleted = true;
-    await reg.save();
-    res.send({
-        data : id,
-        msg : 'Uploaded'
-    })
+    console.log(id);
+    if (!ObjectID.isValid(id)){
+        var reg = await Registration.findById(id);
+        reg.hasUploaded = true,
+        reg.hasCompleted = true;
+        await reg.save();
+        res.send({
+            data : id,
+            msg : 'Uploaded'
+        });
+    }
+    else {
+        res.sendStatus(403);
+    }
+   
 })
 
 module.exports = router;
