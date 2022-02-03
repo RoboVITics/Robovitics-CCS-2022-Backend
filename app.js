056fb24bda4ec4453d31bea7d1973da04cc0693f
Henit Chobisa <henitchobisa0885@gmail.com>
2022-02-03 21:01:06 +0530

    final push

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 const cors = require("cors");
 const db = require("./Config/Database");
 const dotenv = require("dotenv");
